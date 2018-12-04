890c68ba59911949941156d2321241cc12a4c008
albi-c <albert.csontos@gmail.com>
2018-12-04 14:16:46 +0100

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -9,10 +9,10 @@ echo "Copying $BASEDIR/check_alerts_loop.py to $HOME/bin/check_alerts_loop"
 cp $BASEDIR/check_alerts_loop.py $HOME/bin/check_alerts_loop
 chmod 755 $HOME/bin/check_alerts_loop
 
-echo "Copying $BASEDIR/add_timed_alert.py to $HOME/bin/add_timed_alert"
+echo "Copying $BASEDIR/set_timed_alert.py to $HOME/bin/set_timed_alert"
 
-cp $BASEDIR/add_timed_alert.py $HOME/bin/add_timed_alert
-chmod 755 $HOME/bin/add_timed_alert
+cp $BASEDIR/set_timed_alert.py $HOME/bin/set_timed_alert
+chmod 755 $HOME/bin/set_timed_alert
 
 if grep -Fxq "check_alerts_loop" $HOME/.bash_profile; then
 	echo "check_alerts_loop already in $HOME/.bash_profile"
@@ -20,7 +20,7 @@ else
 	echo "Adding check_alerts_loop to $HOME/.bash_profile"
 	
 	cp $HOME/.bash_profile $HOME/.bash_profile_backup
-	echo "check_alerts_loop" >> $HOME/.bash_profile
+	echo "check_alerts_loop&" >> $HOME/.bash_profile
 fi
 
 echo "Installed!"
